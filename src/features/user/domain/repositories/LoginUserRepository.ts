f2dd5e6e80055e568f71711cbdb5385069561a76
Steven Padilla <steven@apx_managed.vanilla>
2023-06-28 08:42:05 -0600

    fix: User login updated

--- src/features/user/domain/repositories/LoginUserRepository.ts
+++ src/features/user/domain/repositories/LoginUserRepository.ts
@@ -1,3 +1,4 @@
+import { User } from '../../domain/entities/User';
 export interface LoginUserRepository {
-    login(password: string, emailOrId_name:string): Promise<boolean>;
+    login(password: string, emailOrId_name:string): Promise<[User,boolean]>;
 }
--- src/features/user/infraestructure/repositoriesImpl/LoginRepositoryImpl.ts
+++ src/features/user/infraestructure/repositoriesImpl/LoginRepositoryImpl.ts
@@ -1,21 +1,27 @@
 import { LoginUserRepository } from "../../domain/repositories/LoginUserRepository";
 import { User } from "../../domain/entities/User";
 import bcrypt from "bcrypt";
-import  {Op } from "sequelize";
+import { Op } from "sequelize";
 
 export class LoginUserRepositoryImpl implements LoginUserRepository {
-  async login(password: string, emailOrId_name: string): Promise<boolean> {
+  async login(
+    password: string,
+    emailOrId_name: string
+  ): Promise<[User, boolean]> {
     let target = new User();
     const res = await User.findOne({
-      where: { 
-        [Op.or]:[
-            {email: emailOrId_name},
-            {id_name: emailOrId_name},
-        ]},
+      where: {
+        [Op.or]: [{ email: emailOrId_name }, { id_name: emailOrId_name }],
+      },
     });
     if (res) target = res;
-    if (!target.id_name) return false;
-    return bcrypt_compare(password, target.password);
+    if (!target.id_name) return [new User(), false];
+    const islogged = await bcrypt_compare(password, target.password);
+    if (islogged){
+      target.password='';
+      return [target, true]
+    }
+    return [target, false];
   }
 }
 
